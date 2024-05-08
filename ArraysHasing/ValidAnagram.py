58d4316de7a49fb076d69ebf096d62dc26d6fc6b
Henry Vu <Vuducdung28@gmail.com>
2024-05-08 16:25:47 -0500

    finished valid anagram

--- ArraysHasing/ValidAnagram.py
+++ ArraysHasing/ValidAnagram.py
@@ -1,13 +1,25 @@
 class Solution(object):
-    def isAnagram(self, s, t):
+    def isAnagram(self, s):
         """
         :type s: str
         :type t: str
         :rtype: bool
-        """
-        
+        """        
+        s = ''.join(ch for ch in s if ch.isalnum()).lower()
+        if s == "":
+            return True
+        b = 0
+        e = len(s)-1
+        print(s)
+        while b <= e:
+            if s[b] != s[e]:
+                return False
+            b += 1
+            e -= 1
+        return True
+    
 if __name__ == "__main__":
     solution = Solution()
-    nums = [1, 2, 3, 1]
-    result = solution.isAnagram(nums)
+    s = "abba"
+    result = solution.isAnagram(s)
     print(result)
