35d0bc97401055acd138e784ba60525ca879c264
Henry Vu <Vuducdung28@gmail.com>
2024-05-08 10:44:45 -0500

    added local test

--- ArraysHasing/ContainsDup.py
+++ ArraysHasing/ContainsDup.py
@@ -12,4 +12,11 @@ class Solution(object):
                 freq[i] = 0
         
         return False
-        
+
+
+if __name__ == "__main__":
+
+    solution = Solution()
+    nums = [1, 2, 3, 1]
+    result = solution.containsDuplicate(nums)
+    print(result)
