7a971f4ad500dfa5bbd204e3a253aa4fa42186dc
Henry Vu <Vuducdung28@gmail.com>
2024-05-08 10:47:20 -0500

    added valid anagram

---
+++ ArraysHasing/ValidAnagram.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [1, 2, 3, 1]
+    result = solution.isAnagram(nums)
+    print(result)
