d198e712212fcb27e556cd4ec5c4be812830e9f6
Henry Vu <Vuducdung28@gmail.com>
2024-05-15 23:30:46 -0600

    updated solution for group anagram

--- ArraysHasing/GroupAnagram.py
+++ ArraysHasing/GroupAnagram.py
@@ -4,7 +4,24 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        
+        if strs == [""]:
+            return [[""]]
+        freq = {}
+        ans = []
+        for i in range(len(strs)):
+            s = [char for char in strs[i]]
+            s.sort()
+            s = "".join(s)
+            if s in freq:
+                freq[s].append(i)
+            else:
+                freq[s] = [i]
+
+        for v in freq.values():
+            group = []
+            for i in v:
+                group.append(strs[i])
+            ans.append(group)
         return ans
             
         
