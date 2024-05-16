07db6fb881d4ed2402d435888043a11f4a936669
Henry Vu <Vuducdung28@gmail.com>
2024-05-16 12:57:53 -0600

    fixed pow

--- Math/Pow.py
+++ Math/Pow.py
@@ -7,17 +7,20 @@ class Solution(object):
         r = 1
         b = x
         e = n
-        
+        if n <= 0:
+            e = 0-n
         while e > 0:
             if e % 2 == 1:
                 r *= b
             b *= b
             e //= 2
+        if n <= 0:
+            r = 1/r
         return r
     
 if __name__ == "__main__":
     solution = Solution()
     x = 2.00000
-    n = 10
+    n = -2
     result = solution.myPow(x, n)
     print(result)
