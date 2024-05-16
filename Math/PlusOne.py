ac0494b9dc1488b554ce1913acf3b9eac0b8e0a9
Henry Vu <Vuducdung28@gmail.com>
2024-05-16 12:51:11 -0600

    faster plusone sol

--- Math/PlusOne.py
+++ Math/PlusOne.py
@@ -4,8 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        n = int("".join(map(str,digits))) + 1
-        return [int(x) for x in str(n)]
+        for i in range(len(digits)-1, -1, -1):
+            print("index", i)
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] = digits[i] + 1
+                return digits
+        return [1] + digits 
     
 if __name__ == "__main__":
     solution = Solution()
