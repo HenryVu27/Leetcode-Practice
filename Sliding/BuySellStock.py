aeedcc9faa2bd7a77507fcc231c46a4ea0192545
Henry Vu <Vuducdung28@gmail.com>
2024-05-17 10:39:43 -0600

    fasted buysell

--- Sliding/BuySellStock.py
+++ Sliding/BuySellStock.py
@@ -1,17 +1,13 @@
 class Solution(object):
     def maxProfit(self, prices):
-        r = 1
-        l = 0
-        prof = 0
-        if len(prices) == 1:
-            return 0
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                prof = max(prof, prices[r] - prices[l])
-            else:
-                l = r
-            r += 1        
-        return prof
+        buy = prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif prices[i] - buy > profit:
+                profit = prices[i] - buy
+        return profit
     
 if __name__ == "__main__":
     solution = Solution()
