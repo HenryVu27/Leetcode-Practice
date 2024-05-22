c146ce9039e93f6c23116eee4f180a01469d0131
Henry Vu <Vuducdung28@gmail.com>
2024-05-21 21:59:53 -0600

    added stack and valid parenthesis

---
+++ Stack/ValidParent.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        check = {')':'(', ']':'[', '}':'{'}
+        for i in s:
+            if i in check:
+                if stack and stack[-1] == check[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True
+            
+        
+    
+if __name__ == "__main__":
+    solution = Solution()
+    s = "({{{}}})"
+    result = solution.isValid(s)
+    print(result)
